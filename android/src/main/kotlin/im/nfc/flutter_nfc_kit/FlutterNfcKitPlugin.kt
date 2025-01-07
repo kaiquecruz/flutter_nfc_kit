b2b9d2a922a745dfd713717c616aae05f0499a0b
kaiquecruz <kaique--batista@hotmail.com>
2025-01-07 15:02:11 -0300

    Update FlutterNfcKitPlugin.kt

--- android/src/main/kotlin/im/nfc/flutter_nfc_kit/FlutterNfcKitPlugin.kt
+++ android/src/main/kotlin/im/nfc/flutter_nfc_kit/FlutterNfcKitPlugin.kt
@@ -25,9 +25,9 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-//import io.flutter.plugin.common.EventChannel
-//import io.flutter.plugin.common.EventChannel.EventSink
-//import io.flutter.plugin.common.EventChannel.StreamHandler
+import io.flutter.plugin.common.EventChannel
+import io.flutter.plugin.common.EventChannel.EventSink
+import io.flutter.plugin.common.EventChannel.StreamHandler
 import org.json.JSONArray
 import org.json.JSONObject
 import java.io.IOException
